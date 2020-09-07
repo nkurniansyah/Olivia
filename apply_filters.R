a351603e1fa7ef970e1daaed17febd6f42144d8b
Tamar Sofer <tamartsi@users.noreply.github.com>
2020-09-07 16:52:27 -0400

    re-wrote function apply_filters
    Added messages, unified naming conventions.

--- apply_filters.R
+++ apply_filters.R
@@ -3,8 +3,8 @@
 
 #' Title Filter transcripts by applying various filters
 #'
-#' @param count_matrix: raw counts (After harmonization based on Phenotype) Rownames count_matrix has to be identical with phnotype ID
-#'                       - rownames must be ENSMBL ID :
+#' @param count_matrix: A p \times n matrix of counts of p transcripts across n individuals. 
+#' 
 #'                         example :
 #'                         TOR841324 TOR127830 TOR257836 TOR461713 TOR508155
 #'      ENSG00000000003        15        20        16         6         9      
@@ -12,21 +12,38 @@
 #'      ENSG00000000419       659       832       855       704       564
 #'      ENSG00000000457       595       946       721       656       555
 #'      
-#' @param cv : Covariance variation (SD/mean)
-#' @param median_count  : median
-#' @param mmr : Max/media ----> inf value will replace to 0
-#' @param percent_zero_count  : percentage of 0 allowed in gen count (count_matrix)
-#' @param range : max-min 
-#' @param pheno  : phenotype after harmonizations
+#' @param cv_min : minimum allowed value of coefficient of variation. Default is NULL.
+#' @param cv_max : maximum allowed value of coefficient of variation. Default is NULL.
+#' @param expression_sum_min  : minimum allowed sum of counts across individuals. Default is 10. 
+#' @param median_min : minimum allowed median count value in the sample. Default is 1. 
+#' @param max_to_median_max  : maximum allowed ratio between the maximum to the 
+#'           median expression count in the sample for a transcript. Default is NULL.
+#' @param max_min: minimum allowed maximum value in the sample for a gene. Default is 5.
+#' @param range_min: minimum allowed range of counts in the sample. Default is 5.
+#' @param prop_zero_max: maximum allowed proportion of individuals with count of 
+#'                               zero for a transcript. Default is 0.8.
 #'
-#' @return (filter gene count or matrix counts )
+#' @return Matrix of transcript counts, with filtered (reduced) rows.
 
-apply_filters <- function(count_matrix, cv_max =NULL, cv_min=NULL, median_val= NULL,mmr_val=NULL, percent_zero_count= NULL,range_val=NULL,Q1_val=NULL, Q3_val=NULL, IQR_val=NULL ,pheno){
+apply_filters <- function(count_matrix, median_min = 1, expression_sum_min = 10, 
+                          max_min = 5, range_min = 5, prop_zero_max = 0.8,
+                          cv_min = NULL, cv_max = NULL, 
+                          max_to_median_max = NULL){
+  
+  message(paste("applying filters on a transcript count matrix of", 
+                nrow(count_matrix), "transcripts, across", ncol(count_matrix), "individuals"))
+ 
+  # check missingness
+  na_inds <- which(is.na(count_matrix))
+  if (length(na_inds) > 0){
+    messsage(paste("There are", length(na_inds), "missing values, setting them to zero"))
+    count_matrix[na_inds] <- 0
+  }
   
   
   #normalize the data
   # TS: which function is this? also, a function should be called "normalize", not "normalized". 
-  norm_count<- normalized(count_matrix) 
+  count_matrix <- normalize(count_matrix) 
   
   ## all parameter should be computed and only then filters should be applied. Because applying
   ## a single filter will change the values computed for the next filter.
@@ -35,128 +52,78 @@ apply_filters <- function(count_matrix, cv_max =NULL, cv_min=NULL, median_val= N
   ############ Compute characteristics of the normalized count matrix. 
   ## TS: why of the normalized? is that what we decided? 
   
+  message("Computing transtripts characteristics...")
+  median_vals <- apply(count_matrix, 1, median)
+  expression_sum_vals <- rowSums(count_matrix)
+  max_vals <- rowSums(count_matrix)
+  range_vals <- apply(count_matrix, 1, function(x) max(x) - min(x))
+  prop_zero_vals <- apply(count_matrix, 1, function(x) mean(x == 0))
+  max_to_median_ratio_vals <- apply(count_matrix, 1, function(x) max(x)/median(x))
+  cv_vals <- apply(count_matrix, 1, function(x) sd(x)/mean(x))
   
-  zero_count<- rowSums(norm_count == 0)
-  
-  zero_count<- data.frame(zero_count= zero_count) %>% rownames_to_column(var="gene_id")
-  
-  #Median
-  median_count<- apply(norm_count, 1, median)
-  
-  median_count<- data.frame(median_count= median_count) %>%rownames_to_column(var="gene_id")
-  
-  # Maximum count
-  max_count<- apply(norm_count, 1, max)
-  max_count<- data.frame(max_count= max_count)%>%rownames_to_column(var="gene_id")
-  
-  #std deviation count
-  sd_count<- apply(norm_count, 1, sd)
-  sd_count<- data.frame(sd_count= sd_count)%>%rownames_to_column(var="gene_id")
-  
-  #Mean count
-  mean_count<- apply(norm_count, 1, mean)
-  mean_count<- data.frame(mean_count= mean_count)%>%rownames_to_column(var="gene_id")
-  
-  # Minimum Count
-  min_count<- apply(norm_count, 1, min)
-  min_count<- data.frame(min_count= min_count)%>%rownames_to_column(var="gene_id")
-  
-  
-  #Q1
-  Q1_count<- apply(norm_count, 1, quantile, prob=0.25) 
-  Q1_count<- data.frame(Q1= Q1_count)%>%rownames_to_column(var="gene_id")
-  
-  Q3_count<- apply(norm_count, 1, quantile, prob=0.75) 
-  Q3_count<- data.frame(Q3= Q3_count)%>%rownames_to_column(var="gene_id")
-  
-  IQR_count<-apply(norm_count, 1, IQR) 
-  IQR_count<- data.frame(IQR= IQR_count)%>%rownames_to_column(var="gene_id")
-  
-  
-  
-  gene_count<- rownames_to_column(as.data.frame(norm_count), var = "gene_id")
-  head(gene_count)
-  
-  #join all for median, mean, max, min, zero count ,and sd
-  gene_summary<- join_all(list(gene_count,zero_count,median_count,max_count, sd_count,mean_count,min_count,Q1_count,Q3_count,IQR_count), by="gene_id", type="left")
-  dim(gene_summary)
-  
-  #gene_summary[,464:469]
-  
-  gene_summary<- gene_summary %>% dplyr::mutate(cv=sd_count/mean_count ) %>%
-    dplyr::mutate(mmr=max_count/median_count ) %>%
-    dplyr::mutate(range=max_count-min_count)
-  
-  
-  ## This Early filter to remove lowly express (remove all the gene which have max value < 10)
-  gene_summary<-gene_summary[which(gene_summary$max_count > 10),]
-  dim(gene_summary)
-  
-  if(!is.null(cv_max)){
-    #remove median below 5
-    counts_cv_max<-gene_summary[which(gene_summary$cv <= cv_max),]
-    dim(counts_cv_max)
-    gene_summary<- counts_cv_max
-    
+  message("Appying filters...")
+  inds_rm <- c()
+  if (!is.null(median_min)){
+    inds_median_min <- which(median_vals < median_min)
+    message(paste("There are", length(inds_median_min), "transcripts with median 
+                  value lower than", median_min))
+    inds_rm <- c(inds_rm, inds_median_min)
   }
   
-  if(!is.null(cv_min)){
-    #remove median below 5
-    counts_cv_min<-gene_summary[which(gene_summary$cv >= cv_min),]
-    dim(counts_cv_min)
-    gene_summary<- counts_cv_min
-    
+  if (!is.null(expression_sum_min)){
+    inds_expression_sum_min <- which(expression_sum_vals < expression_sum_min)
+    message(paste("There are", length(inds_expression_sum_min), "transcripts with expression sum 
+                  value lower than", expression_sum_min))
+    inds_rm <- c(inds_rm, inds_expression_sum_min)
   }
   
-  if(!is.null(percent_zero_count)){
-    
-    counts_nonzero<-gene_summary[which(gene_summary$zero_count<= percent_zero_count*nrow(pheno)),]
-    gene_summary<- counts_nonzero
-    #
+  if (!is.null(max_min)){
+    inds_max_min <- which(max_vals < max_min)
+    message(paste("There are", length(inds_max_min), "transcripts with maximum expression
+                  value lower than", max_min))
+    inds_rm <- c(inds_rm, inds_max_min)
   }
-  #Filter Median
-  if(!is.null(median_val)){
-    counts_med<-gene_summary[which(gene_summary$median_count >= median_val),]
-    dim(counts_med)
-    gene_summary<- counts_med
-    
-  }
-  ## MMR
-  if(!is.null(mmr_val)){
-    gene_summary$mmr[is.infinite(gene_summary$mmr)] <- 0
-    counts_mmr<- gene_summary[which(gene_summary$mmr < mmr_val),]
-    gene_summary<- counts_mmr
-    
-  }
-  ### range
-  if(!is.null(range_val)){
-    counts_range<- gene_summary[which(gene_summary$range < range_val),]
-    gene_summary<- counts_range
-    
+
+  if (!is.null(range_min)){
+    inds_range_min <- which(range_vals < range_min)
+    message(paste("There are", length(inds_range_min), "transcripts with maximum 
+                  expression range value lower than", range_min))
+    inds_rm <- c(inds_rm, inds_range_min)
   }
   
-  if(!is.null(Q1_val)){
-    counts_Q1<- gene_summary[which(gene_summary$Q1 > Q1_val ),]
-    gene_summary<- counts_Q1
-    
+  if (!is.null(prop_zero_max)){
+    inds_prop_zero_max <- which(prop_zero_vals > prop_zero_max)
+    message(paste("There are", length(inds_prop_zero_max), "transcripts with propotion 
+                  of zero counts higher than", prop_zero_max))
+    inds_rm <- c(inds_rm, inds_prop_zero_max)
   }
   
-  if(!is.null(Q3_val)){
-    counts_Q3<- gene_summary[which(gene_summary$Q3 < Q3_val ),]
-    gene_summary<- counts_Q3
-    
-  }
+  if (!is.null(cv_min)){
+    inds_cv_min <- which(cv_vals < cv_min)
+    message(paste("There are", length(inds_cv_min), "transcripts with coefficient of 
+                  variation lower than", cv_min))
+    inds_rm <- c(inds_rm, inds_cv_min)
+  } 
+
+  if (!is.null(cv_max)){
+    inds_cv_max <- which(cv_vals < cv_max)
+    message(paste("There are", length(inds_cv_max), "transcripts with coefficient of 
+                  variation higher than", cv_max))
+    inds_rm <- c(inds_rm, inds_cv_max)
+  }   
   
-  if(!is.null(IQR_val)){
-    counts_IQR<- gene_summary[which(gene_summary$IQR < IQR_val ),]
-    gene_summary<- counts_IQR
-    
-  }
+  if (!is.null(max_to_median_max)){
+    inds_max_to_median_max <- which(max_to_median_vals > max_to_median_max)
+    message(paste("There are", length(inds_max_to_median_max), "transcripts with coefficient of 
+                  variation higher than", max_to_median_max))
+    inds_rm <- c(inds_rm, inds_max_to_median_max)
+  }   
+   
+  inds_rm <- unique(inds_rm)
+  message(paste("Removing", length(inds_rm), "unique transcripts not passing requested filters"))
   
-  print(dim(gene_summary))
   
-  
-  counts<- norm_count[gene_summary$gene_id,]
-  return(counts)
+  count_matrix <- count_matrix[-inds_rm, ]
+  return(count_matrix)
 }
 
