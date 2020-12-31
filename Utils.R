ddf25001583b53ce8abe54d9f8ee1954a4eec205
nkurniansyah <nuzulul.kurniansyah1992@gmail.com>
2020-12-31 08:29:57 -0500

    Add storey

--- Utils.R
+++ Utils.R
@@ -126,6 +126,12 @@ quantile_empirical_pvalue <- function(p_values, null_p_values){
 
 
 
+storey_empirical_pvalue <- function(z_score, null_z_score){
+  emp_pvalues<- empPvals(z.score, null_zscore_res)
+  emp_pvalues
+}
+
+
 
 #' Title: Residual permutation for continuous trait
 #'
