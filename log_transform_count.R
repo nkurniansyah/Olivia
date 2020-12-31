1526a8850c6d39ea7b44a5fb6b80aa5334d0f59c
Tamar Sofer <tamartsi@users.noreply.github.com>
2020-12-31 10:04:03 -0500

    Update log_transform_count.R
    Put all log transfromation functions in the same file

--- log_transform_count.R
+++ log_transform_count.R
@@ -34,3 +34,46 @@ log_transform_count <- function(count_matrix, transform = "log_replace_half_min"
    
   
 }
+
+
+
+
+#' Title: Log Transform method (Replace zero with minimum value for a transcript divided by 2)
+#'
+#' @param count_matrix : A p x n matrix of gene expression counts. p are genes, n are individuals. Rownames are gene names
+#'
+#' @return : matrix of gene expression counts after transformation
+
+
+log_replace_half_min<- function(count_matrix){
+  imputed_mat <- t(apply(count_matrix,1,function(x){x[x==0] <- min(x[x>0]/2);x}))
+  imputed_mat <- log2(imputed_mat)
+  return(imputed_mat)
+}
+
+
+#' Title:  Log Transform method (Add 0.5 to entire matrix)
+#'
+#' @param count_matrix  A p x n matrix of gene expression counts. p are genes, n are individuals. Rownames are gene names
+#'
+#' @return : matrix of gene expression counts after transformation
+#' 
+
+log_add_0.5<- function(count_matrix){
+  imputed_mat <- log2(count_matrix + 0.5)
+  return(imputed_mat)
+}
+
+
+
+#' Title: Log Transform method (Add min value divided by 2 to entire matrix gene expression counts)
+#'
+#' @param count_matrix  A p x n matrix of gene expression counts. p are genes, n are individuals. Rownames are gene names
+#'
+#' @return : matrix of gene expression counts after transformation
+#' 
+log_add_min <-  function(count_matrix){
+  imputed_mat = t(apply(count_matrix,1,function(x){x= x + min(x[x>0]/2);x}))
+  imputed_mat <- log2(imputed_mat)
+  return(imputed_mat)
+}
